76c257d455a7d7a3b357ede0a5a3582862437f17
Alexander Winter <a.w1nter@hotmail.com>
2024-12-01 13:56:45 -0500

    Pinyinify working (on linux)

---
+++ src/main/java/com/winteralexander/hanzirp/Pinyinify.java
@@ -0,0 +1,56 @@
+package com.winteralexander.hanzirp;
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.SortedMap;
+
+/**
+ * TODO Undocumented :(
+ * <p>
+ * Created on 2024-11-30.
+ *
+ * @author Alexander Winter
+ */
+public class Pinyinify {
+	public static void main(String[] args) throws Throwable {
+		Gson gson = new Gson();
+
+		翻译Reader reader = new 翻译Reader(gson);
+
+		SortedMap<String, String> map = reader.读翻译(new File("in/translation/zh_cn.json"));
+
+		StringBuilder sb = new StringBuilder();
+
+		for(SortedMap.Entry<String, String> entry : map.entrySet()) {
+			String key = entry.getKey();
+			if(!key.startsWith("block.minecraft"))
+				continue;
+
+			String value = entry.getValue();
+			sb.append(value).append(",");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		String[] res = pinyinify(sb.toString()).split(",");
+		int i = 0;
+		for(String key : map.keySet()) {
+			map.put(key, res[i].trim().replaceAll("(\\s|​)+", " "));
+			i++;
+		}
+
+		reader.写翻译(new File("out/pinyin.json"), map);
+	}
+
+	public static String pinyinify(String hanzi) throws Throwable {
+		ProcessBuilder pb = new ProcessBuilder();
+		pb.command("bash", "-c",
+				"echo \"h=require('hanzi-tools');console.log(h.pinyinify('" + hanzi + "'));" +
+						"\" > out/hanzi.js; node out/hanzi.js");
+		File file = new File("out/hanzi.txt");
+		pb.redirectOutput(file);
+		Process p = pb.start();
+		p.waitFor();
+		return Files.readString(file.toPath()).trim();
+	}
+}
--- src/main/java/com/winteralexander/hanzirp/翻译Reader.java
+++ src/main/java/com/winteralexander/hanzirp/翻译Reader.java
@@ -1,13 +1,19 @@
 package com.winteralexander.hanzirp;
 
+import com.google.gson.FormattingStyle;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.sun.source.tree.Tree;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * TODO Undocumented :(
@@ -23,16 +29,30 @@ public class 翻译Reader {
 		this.gson = gson;
 	}
 
-	public Map<String, String> 读翻译(File file) throws IOException {
+	public SortedMap<String, String> 读翻译(File file) throws IOException {
 		JsonReader reader = gson.newJsonReader(new FileReader(file));
-		Map<String, String> hashMap = new HashMap<>();
+		SortedMap<String, String> hashMap = new TreeMap<>();
 		reader.beginObject();
 		while(reader.hasNext()) {
 			String key = reader.nextName();
 			String value = reader.nextString();
 			hashMap.put(key, value);
 		}
+		reader.close();
 
 		return hashMap;
+	}
+
+	public void 写翻译(File file, SortedMap<String, String> map) throws IOException {
+		JsonWriter writer = gson.newJsonWriter(new FileWriter(file));
+		writer.setFormattingStyle(FormattingStyle.PRETTY);
+		writer.beginObject();
+		for(Map.Entry<String, String> entry : map.entrySet()) {
+			writer.name(entry.getKey());
+			writer.value(entry.getValue());
+		}
+
+		writer.endObject();
+		writer.close();
 	}
 }
